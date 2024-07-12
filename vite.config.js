3b381665b8b1598d06eeb5a22f3efeccfd15711a
Raveesha Vishwajith <107930676+raveeshavishwajith@users.noreply.github.com>
2024-07-12 06:30:55 +0530

    remove sentry package

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,9 @@
-import { sentryVitePlugin } from "@sentry/vite-plugin";
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.ts
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), sentryVitePlugin({
-    org: "digital-dreamers-al",
-    project: "javascript-react"
-  })],
-  base: "/vite-react-deploy/",
-
-  build: {
-    sourcemap: true
-  }
-})
+  plugins: [react()],
+  base: "/vite-react-deploy/", // YOUR REPO NAME HERE
+});
