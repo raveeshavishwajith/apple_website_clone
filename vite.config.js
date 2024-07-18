c37998ab5b214554cba8931806b7f6c8d3bea957
Raveesha Vishwajith <107930676+raveeshavishwajith@users.noreply.github.com>
2024-07-18 13:22:29 +0530

    modification

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,18 @@
-// vite.config.ts
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+// // vite.config.ts
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   base: "/vite-react-deploy/", // YOUR REPO NAME HERE
+// });
+
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/vite-react-deploy/", // YOUR REPO NAME HERE
-});
+})
